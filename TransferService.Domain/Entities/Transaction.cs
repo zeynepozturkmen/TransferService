b2142957191e522033b63e6d3d802abef60e1d1b
unknown <zeynepozturkmen62@gmail.com>
2025-10-03 23:54:55 +0300

    Remove transactionCode data annotation

--- TransferService.Domain/Entities/Transaction.cs
+++ TransferService.Domain/Entities/Transaction.cs
@@ -11,7 +11,7 @@ namespace TransferService.Domain.Entities
         public decimal USDAmount { get; set; }
         public string Currency { get; set; } = "TRY";
         public decimal Fee { get; set; }
-        public string TransactionCode { get; set; } = Guid.NewGuid().ToString().Substring(0, 8).ToUpper();
+        public string TransactionCode { get; set; }
         public TransferStatus Status { get; set; } = TransferStatus.Pending;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? CompletedAt { get; set; }
